f7d72f36ae93fa1f4cf4d749acdb32e0059aedaf
Sergiy Misyura <sergey@cando.com>
2013-05-13 17:51:07 -0400

    Fixed bug for nested templates

--- transformations/templates.js
+++ transformations/templates.js
@@ -1,16 +1,28 @@
 (function($, app) {
 
-	app.transformation('[data-template]', function($element) {
+	var templateTransformation = function($element) {
+		// We don't need to process it if it is detached from the body.
+		if ($element.parents('body').length == 0) {
+			return;
+		}
+		$element.find('[data-template]').each(function(idx, el) {
+			templateTransformation($(el));
+		});
 		var $placeholder = $("<div />").addClass($element.attr('data-template') + '-placeholder');
 		var controllerElement = $element.closest('[data-controller]').get(0);
-		if (typeof controllerElement.templates == 'undefined') {
-			controllerElement.templates = [];
+		if (controllerElement)
+		{
+			if (typeof controllerElement.templates == 'undefined') {
+				controllerElement.templates = [];
+			}
+			$element.replaceWith($placeholder);
+			controllerElement.templates[$element.attr('data-template')] = { 
+				template: $("<div />").append($element),
+				placeholder: $placeholder
+			}
 		}
-		$element.replaceWith($placeholder);
-		controllerElement.templates[$element.attr('data-template')] = { 
-			template: $("<div />").append($element),
-			placeholder: $placeholder
-		}
-	});
+	};
+
+	app.transformation('[data-template]', templateTransformation);
 
 })(jQuery, app);
