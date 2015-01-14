aac7db032d99102b9f4636dc32f75e2a1540700a
Sergiy Misyura <sergey@cando.com>
2015-01-14 17:19:25 -0500

    A list of files in php format

---
+++ files.php
@@ -0,0 +1,12 @@
+<?php
+return [
+	'app.js',
+	'init.js',
+	'services/api.js',
+	'services/bind.js',
+	'services/deferred.js',
+	'services/rendering.js',
+	'services/form.js',
+	'transformations/templates.js',
+	'transformations/tiny.js',
+];
