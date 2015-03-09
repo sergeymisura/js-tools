db8d1c860fb51c359e8d32c7e9ca2a8676d0a9e1
Sergiy Misyura <sergey@cando.com>
2015-03-09 09:46:05 -0400

    Updated services.api

--- services/api.js
+++ services/api.js
@@ -1,70 +1,89 @@
 /* This module provide some wrapping around JSON-based AJAX calls */
 (function() {
 
-	/* A factory for the internal error handler */
-	var _errorCallbackFactory = function(result) {
-		return function(xhr, textStatus, errorThrown) {
-			var data = xhr.responseJSON;
-
-			$(app).trigger('api.error', [xhr.status, data, xhr]);
-
-			if (result.triggerError(xhr.status, data, xhr)) {
-				return;
-			}
-
-			/* If the error wasn't processed by the .error() callback, we trigger a global status */
-			$(app).trigger('api.error.unhandled', [xhr.status, data, xhr]);
-		}
-	};
-
 	var serviceFactory = function($element, services) {
+
+		/* A factory for the internal error handler */
+		var _errorCallbackFactory = function(result) {
+			return function(xhr, textStatus, errorThrown) {
+				var data = xhr.responseJSON;
+
+				$(app).trigger('api.error', [xhr.status, data, xhr]);
+
+				if (result.triggerError(xhr.status, data, xhr)) {
+					return;
+				}
+
+				/* If the error wasn't processed by the .error() callback, we trigger a global status */
+				$(app).trigger('api.error.unhandled', [xhr.status, data, xhr]);
+			}
+		};
+
+		var _request = function(uri, data, verb, options) {
+			options = options || {};
+			data = data || {};
+
+			var result = services.deferred();
+			$.ajax(
+				app.config.baseUrl + app.config.apiUrl + '/' + uri,
+				$.extend(
+					{
+						type: verb,
+						data: data,
+						success: result.successCallback(),
+						error: _errorCallbackFactory(result)
+					},
+					options
+				)
+			);
+			return result;
+
+		};
+
+		/* A wrapper HTTP calls made with a json body (normally put or post) */
+		var _jsonBodyRequest = function(uri, data, verb){
+			return _request(
+				uri,
+				JSON.stringify(data),
+				verb,
+				{
+					contentType: 'application/json',
+					dataType: 'json'
+				}
+			);
+		};
+
 		return {
 
 			/* A wrapper for HTTP GET call */
 			get: function(uri, data) {
-				if (typeof data == 'undefined') {
-					data = {}
-				}
-				var result = services.deferred();
-				$.ajax(
-					app.config.baseUrl + app.config.apiUrl + '/' + uri,
-					{
-						data: data,
-						success: result.successCallback(),
-						error: _errorCallbackFactory(result)
-					}
-				);
-				return result;
+				return _request(uri, data, 'GET');
+			},
+
+			/* A wrapper for HTTP DELETE call */
+			'delete': function(uri){
+				return _request(uri, null, 'DELETE');
 			},
 
 			/* A wrapper for HTTP POST call */
 			post: function(uri, data) {
-				return this._jsonBodyRequest(uri, data, 'post');
+				return _jsonBodyRequest(uri, data, 'post');
 			},
 
 			/* A wrapper for HTTP PUT call */
 			put: function(uri, data) {
-				return this._jsonBodyRequest(uri, data, 'put');
+				return _jsonBodyRequest(uri, data, 'put');
 			},
 
-			/* A wrapper HTTP calls made with a json body (normally put or post) */
-			_jsonBodyRequest: function(uri, data, verb){
-				if (typeof data == 'undefined') {
-					data = {}
+			/* Performs POST request to create a new resource (id is null) or PUT to update an existing one (id is not
+			null) */
+			save: function(url, id, data) {
+				if (id) {
+					return this.put(url + '/' + id, data);
 				}
-				var result = services.deferred();
-				$.ajax(
-					app.config.baseUrl + app.config.apiUrl + '/' + uri,
-					{
-						type: verb,
-						contentType: 'application/json',
-						dataType: 'json',
-						data: JSON.stringify(data),
-						success: result.successCallback(),
-						error: _errorCallbackFactory(result)
-					}
-				);
-				return result;
+				else {
+					return this.post(url, data);
+				}
 			}
 
 		};
