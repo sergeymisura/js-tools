9809de6de09d3ba611011e2b7c570babeef66161
Sergiy Misyura <sergey@cando.com>
2015-03-09 10:02:04 -0400

    Updated services.hash

--- services/hash.js
+++ services/hash.js
@@ -92,93 +92,114 @@
 			$.each(_map, function(idx, pair) { _test(pair[0], pair[1]); });
 		};
 
-		var service =  {
 
+		return $.extend(
 			/**
-			 * Regular expression that can be used to attach a handler for the empty URL hash.
-			 */
-			EMPTY: /^$/,
-
-			/**
-			 * Regular expression that can be used to attach a handler for every URL hash change.
-			 */
-			ALL: /.*/,
-
-			/* Builds the hash portion of the URL by combining the first array into the path separated by '/' and using
-			 * the second optional argument as a collection of query string parameters.
-			 *
-			 * services.hash.build(['user', 2], { display: 'all' }) will return '#users/2?display=all' */
-			build: function(path, params) {
-				if (path.constructor == Array) {
-					path = path.join('/');
-				}
-
-				var hash = '#' + path;
-				if (params) {
-					var paramsArray = [];
-					$.each(params, function(key, value) {
-						if (value == '') {
-							paramsArray.push(encodeURIComponent(key));
-						}
-						else {
-							paramsArray.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
-						}
-					});
-					if (paramsArray.length > 0) {
-						hash += '?' + paramsArray.join('&');
-					}
-				}
-				return hash;
-			},
-
-			/* Builds and applies the hash portion of the URL (see hash.build()) */
-			go: function(path, params) {
-				document.location = this.build(path, params);
-			},
-
-			/**
-			 * Sets the new value for one of the parameters and returns a new URL.
-			 *
-			 * @param {string} param  Parameter to set
-			 * @param {string} value  New value
-			 *
-			 * @returns {string}  Updated URL
-			 */
-			set: function(param, value) {
-				var query = $.extend({}, _parsedHash.query);
-				query[param] = value || '';
-				return this.build(_parsedHash.path, query);
-			},
-
-			/**
-			 * Adds a new URL changing handlers. The handler will be evoked if the new url matches the provided pattern.
+			 * Being called as a function, the service adds URL changing event handlers. The function accepts an array
+			 * of pattern - handler pairs. The handler will be evoked if the new URL hash matches the pattern.
 			 * If pattern is a string, it will be matched against the first part of the hash (e.g. handler for 'users'
 			 * will be evoked if the new URL is #users/2
 			 *
-			 * @param  pattern
-			 * @param  handler
-			 * @returns {service}
+			 * Sample call:
+			 *
+			 * services.hash(
+			 * 	['users', this.displayUsers],
+			 * 	['contracts', this.displayContracts],
+			 * 	[services.hash.EMPTY, this.displayDefault]
+			 * )
 			 */
-			add: function(pattern, handler) {
-
+			function() {
 				if (!_handlerInstalled) {
 					_parse();
 					$(window).on('hashchange', _hashChangeHandler);
 					_handlerInstalled = true;
 				}
 
-				if (typeof handler == 'string') {
-					handler = $element[0].controller[handler];
+				$.each(arguments, function(idx, pair) {
+					var pattern = pair[0];
+					var handler = pair[1];
+
+					if (typeof handler == 'string') {
+						handler = $element[0].controller[handler];
+					}
+
+					_map.push([pattern, handler]);
+
+					_test(pattern, handler);
+				});
+			},
+			{
+				/**
+				 * Regular expression that can be used to attach a handler for the empty URL hash.
+				 */
+				EMPTY: /^$/,
+
+				/**
+				 * Regular expression that can be used to attach a handler for every URL hash change.
+				 */
+				ALL: /.*/,
+
+				/* Builds the hash portion of the URL by combining the first array into the path separated by '/' and using
+				 * the second optional argument as a collection of query string parameters.
+				 *
+				 * services.hash.build(['user', 2], { display: 'all' }) will return '#users/2?display=all' */
+				build: function(path, params) {
+					if (path.constructor == Array) {
+						path = path.join('/');
+					}
+
+					var hash = '#' + path;
+					if (params) {
+						var paramsArray = [];
+						$.each(params, function(key, value) {
+							if (value == '') {
+								paramsArray.push(encodeURIComponent(key));
+							}
+							else {
+								paramsArray.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
+							}
+						});
+						if (paramsArray.length > 0) {
+							hash += '?' + paramsArray.join('&');
+						}
+					}
+					return hash;
+				},
+
+				/* Builds and applies the hash portion of the URL (see hash.build()) */
+				go: function(path, params) {
+					document.location = this.build(path, params);
+				},
+
+				/**
+				 * Sets the new value for one of the parameters and returns a new URL.
+				 *
+				 * @param {string} param  Parameter to set
+				 * @param {string} value  New value
+				 *
+				 * @returns {string}  Updated URL
+				 */
+				set: function(param, value) {
+					var query = $.extend({}, _parsedHash.query);
+					query[param] = value || '';
+					return this.build(_parsedHash.path, query);
+				},
+
+				/**
+				 * Adds a new URL changing handlers. The handler will be evoked if the new url matches the provided pattern.
+				 * If pattern is a string, it will be matched against the first part of the hash (e.g. handler for 'users'
+				 * will be evoked if the new URL is #users/2
+				 *
+				 * @param  pattern
+				 * @param  handler
+				 * @returns {service}
+				 */
+				add: function(pattern, handler) {
+					app.log('Usage of services.hash.add() is deprecated. Use services.hash() instead.');
+					this([pattern, handler]);
+					return this;
 				}
-
-				_map.push([pattern, handler]);
-
-				_test(pattern, handler);
-
-				return this;
 			}
-		};
-
-		return service;
+		);
 	});
 })(jQuery, app);
